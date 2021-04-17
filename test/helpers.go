4b66bf4cf4a966a0abc0bccbcad127f739ef21a7
Joey Espinosa <jlouis.espinosa@gmail.com>
2021-04-17 00:33:08 -0400

    feat: add test package
    this includes helpers.go with helper functions for mocking and
    temporarily changing config location, and matchers.go with custom gomega
    matcher for test purposes

---
+++ test/helpers.go
@@ -0,0 +1,79 @@
+package test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"time"
+
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+
+	kc "github.com/particledecay/kconf/pkg/kubeconfig"
+)
+
+// stores the filepaths for cleanup
+var tmpfiles []string
+
+// MockConfig generates a mock KConf with `num` number of resources
+func MockConfig(num int) *kc.KConf {
+	config := clientcmdapi.NewConfig()
+	for i := 0; i < num; i++ {
+		var name string
+		if i == 0 {
+			name = "test"
+		} else {
+			name = fmt.Sprintf("test-%d", i)
+		}
+		config.Clusters[name] = &clientcmdapi.Cluster{
+			LocationOfOrigin:         "/home/user/.kube/config",
+			Server:                   fmt.Sprintf("https://example-%s.com:6443", name),
+			InsecureSkipTLSVerify:    true,
+			CertificateAuthority:     "bbbbbbbbbbbb",
+			CertificateAuthorityData: []byte("bbbbbbbbbbbb"),
+		}
+		config.AuthInfos[name] = &clientcmdapi.AuthInfo{
+			LocationOfOrigin: "/home/user/.kube/config",
+			Token:            fmt.Sprintf("bbbbbbbbbbbb-%s", name),
+		}
+		config.Contexts[name] = &clientcmdapi.Context{
+			LocationOfOrigin: "/home/user/.kube/config",
+			Cluster:          name,
+			AuthInfo:         name,
+			Namespace:        "default",
+		}
+	}
+	return &kc.KConf{Config: *config}
+}
+
+// WriteTmpConfig writes a kubeconfig to a temporary file instead of the default location
+func WriteTmpConfig(config *kc.KConf) error {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	tmpfile, err := ioutil.TempFile("", fmt.Sprintf("config-%d", r1))
+	if err != nil {
+		return err
+	}
+
+	// for later removal
+	tmpfiles = append(tmpfiles, tmpfile.Name())
+
+	// override the default kubeconfig path
+	kc.MainConfigPath = tmpfile.Name()
+
+	err = config.Save()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// CleanupFiles removes any temporary files created during tests
+func CleanupFiles() {
+	for _, filepath := range tmpfiles {
+		if err := os.Remove(filepath); err != nil {
+			kc.Out.Log().Err(err).Msgf("could not remove '%s'", filepath)
+		}
+	}
+}
---
+++ test/matchers.go
@@ -0,0 +1,53 @@
+package test
+
+import (
+	"fmt"
+
+	"github.com/onsi/gomega/format"
+	gomatchers "github.com/onsi/gomega/matchers"
+	gotypes "github.com/onsi/gomega/types"
+
+	kc "github.com/particledecay/kconf/pkg/kubeconfig"
+)
+
+type ContainContextMatcher struct {
+	Element interface{}
+}
+
+func ContainContext(element interface{}) gotypes.GomegaMatcher {
+	return &ContainContextMatcher{
+		Element: element,
+	}
+}
+
+func (matcher *ContainContextMatcher) Match(actual interface{}) (bool, error) {
+	config, ok := actual.(*kc.KConf)
+	if !ok {
+		return false, fmt.Errorf("ContainContext matcher expects a KConf. Got:\n%s", format.Object(actual, 1))
+	}
+
+	elemMatcher := &gomatchers.EqualMatcher{Expected: matcher.Element}
+
+	var lastError error
+	contexts, _ := config.List()
+	for _, ctx := range contexts {
+		success, err := elemMatcher.Match(ctx)
+		if err != nil {
+			lastError = err
+			continue
+		}
+		if success {
+			return true, nil
+		}
+	}
+
+	return false, lastError
+}
+
+func (matcher *ContainContextMatcher) FailureMessage(actual interface{}) (message string) {
+	return format.Message(actual, "to contain context matching", matcher.Element)
+}
+
+func (matcher *ContainContextMatcher) NegatedFailureMessage(actual interface{}) (message string) {
+	return format.Message(actual, "not to contain element matching", matcher.Element)
+}
