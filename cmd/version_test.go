7f20a721d27e47117faba784547460876e7b7547
Joey Espinosa <jlouis.espinosa@gmail.com>
2021-05-06 00:05:39 -0400

    chore(test): add a version test

---
+++ cmd/version_test.go
@@ -0,0 +1,50 @@
+package cmd_test
+
+import (
+	"io/ioutil"
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/particledecay/kconf/build"
+	"github.com/particledecay/kconf/cmd"
+)
+
+var _ = Describe("Cmd/VersionCmd", func() {
+	It("Should display short version information", func() {
+		// FIXME: Ginkgo supplies its own CLI flags which screws up stdout redirection
+		// see https://github.com/onsi/ginkgo/issues/285#issuecomment-290575636
+		// here we are truncating all args that aren't the command itself
+		origArgs := os.Args[:]
+		os.Args = os.Args[:1]
+
+		// redirect stdout
+		r, w, _ := os.Pipe()
+		oldOut := os.Stdout
+		os.Stdout = w
+
+		// set a fake version
+		build.Version = "1.2.3"
+		build.Commit = "asdfasdfasdf"
+		build.Date = "2020-01-01"
+
+		versionCmd := cmd.VersionCmd()
+		versionCmd.SilenceErrors = true
+		err := versionCmd.Execute()
+
+		Expect(err).NotTo(HaveOccurred())
+
+		// read captured stdout
+		w.Close()
+		out, _ := ioutil.ReadAll(r)
+
+		// restore stdout
+		os.Stdout = oldOut
+
+		// restore args back to what they were
+		os.Args = origArgs[:]
+
+		Expect(out).To(ContainSubstring("1.2.3"))
+		Expect(out).NotTo(ContainSubstring("asdfasdfasdf")) // short version doesn't contain this
+	})
+})
